d6d02036902875698d197114af08da9fd0a8ae67
Alexander <knibz@protonmail.com>
2022-10-08 16:55:28 -0400

    spent some time researching and settled upon the bubble sort method

--- Q5.js
+++ Q5.js
@@ -15,3 +15,17 @@
 // Input: [9, 2, 7, 12]
 
 // Output: [2, 7, 9, 12]
+
+const sorted = () => {
+  const arr = [9, 2, 7, 12, 1]
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      };
+    };
+  };
+  return arr;
+};
